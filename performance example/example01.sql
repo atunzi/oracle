c48d525a5bf1779a413f01266a327a4e05678901
Aron Tunzi <aron.tunzi@swisscom.com>
2026-01-15 15:22:23 +0100

    first version

--- performance example/example01.sql
+++ performance example/example01.sql
@@ -1,1 +1,17 @@
+set lines 150 pages 100
+col tracefile for a90
+
+
+select s.sid,
+       s.serial#,
+       s.status,
+       p.spid,
+       p.tracefile
+from   gv$session s,
+       gv$process p
+where  p.addr = s.paddr and p.inst_id = s.inst_id
+  and  s.sid in (select sid from v$mystat where rownum <= 1);
+
+
+
 show parameter optimizer
